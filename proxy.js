92fc98f35a7aa063b9f8f6f596a576c66d3ea9c1
Mwungere <119742006+Mwungere@users.noreply.github.com>
2024-07-17 19:47:53 +0200

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -121,9 +121,9 @@ function handleError(error, res) {
   }
 }
 // Serve index.html
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
 // Start the server
 app.listen(port, () => {
   console.log(`Proxy server running at http://localhost:${port}`);
